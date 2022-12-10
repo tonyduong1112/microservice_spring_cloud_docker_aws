016ed52b6e2a337b39207328e3644d6945b257bb
Duong Manh Tuan <tony.duong1112@gmail.com>
2022-12-10 14:10:21 +0700

    1. Correct spring cloud config name server 2. Permit all Ipaddress for web security for dev purpose (previously alow only api gateway ip)

--- PhotoAppApiUsers/src/main/java/io/tony/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/io/tony/photoapp/api/users/security/WebSecurity.java
@@ -26,7 +26,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"))
+//        http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"))
+        http.authorizeRequests().antMatchers("/**").permitAll()
                 .and()
                 .addFilter(getAuthenticationFilter());
         http.headers().frameOptions().disable(); // to show h2-console
